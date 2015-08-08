7d66c0f125f74096b323c38c59b8ca8edb92021a
William Casper <riley_c2003@yahoo.com>
2015-08-08 13:13:13 -0700

    fixed typo in init.c

--- src/init.c
+++ src/init.c
@@ -14,27 +14,27 @@ int init() {
 
   // initialize MPI task numbers
   status = comm_init();
-  error_check(&status, "error_in comm_init\n");
+  error_check(&status, "error in comm_init\n");
   if(status) return status;
 
   // initialize grid
   status = grid_init();
-  error_check(&status, "error_in grid_init\n");
+  error_check(&status, "error in grid_init\n");
   if(status) return status;
 
   // initialize fourier
   status = fourier_init();
-  error_check(&status, "error_in fourier_init\n");
+  error_check(&status, "error in fourier_init\n");
   if(status) return status;
 
   // initialize state
   status = state_init();
-  error_check(&status, "error_in state_init\n");
+  error_check(&status, "error in state_init\n");
   if(status) return status;
 
   // initialize model
   status = model_init();
-  error_check(&status, "error_in model_init\n");
+  error_check(&status, "error in model_init\n");
   if(status) return status;
 
   return status;
