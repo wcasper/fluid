ae294491412880ce174590f8ba4d9382c8679929
wcasper <wcasper@lanl.gov>
2015-08-11 19:06:29 -0600

    fixed time stepping routine

--- src/time.c
+++ src/time.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 #include <iniparser.h>
 #include <mpi.h>
 
@@ -7,6 +8,8 @@
 #include "comm.h"
 #include "config.h"
 #include "error.h"
+#include "diag.h"
+#include "state.h"
 
 double time = 0.0;
 double time_dt = 1e-0;
@@ -16,6 +19,7 @@ double time_err_max = 1e-8;
 double (*time_step_model)(double, double);
 
 static int time_read_config();
+static double time_getmax(double complex *kfield);
 
 int time_step_set(double (*step_function)(double, double)) {
   time_step_model = step_function;
@@ -63,12 +67,38 @@ int time_init() {
   return status;
 }
 
+double time_getmax(double complex *kfield) {
+  int idx2d, idx3d, m;
+
+  double u, umax;
+
+  umax = 0.0;
+  for(idx2d = 0; idx2d < grid_2d_nn_local; idx2d++) {
+    if(!grid_2d_dealias_mask[idx2d]) {
+      for(m = 0; m < (grid_nz*2)/3; m++) {
+        idx3d = idx2d + grid_2d_nn_local*m;
+
+        u = cabs(kfield[idx3d]);
+
+        umax = (u < umax) ? umax : u;
+      }
+    }
+  }
+
+  return umax;
+}
+
+
 int time_step() {
   double err = 0.0,
          t1  = time + time_dt,
-         dt;
+         dt, factor;
 
   bool is_boundary_step;
+
+  double umax, vmax, wmax, bmax;
+
+  diag_write();
 
   while(time < t1) {
     if(time + time_step_dt > t1) {
@@ -79,22 +109,34 @@ int time_step() {
       dt = time_step_dt;
       is_boundary_step = false;
     }
-    err = time_err_max + 1.0;
-    while(err >= time_err_max) {
+    factor = 10.0;
+    while(factor > 1.0) {
       err = time_step_model(dt,time_err_max);
-      printf("step err = %1.16lf, step size = %lf\n", err, dt);
+      //printf("step err = %1.16lf, step size = %lf\n", err, dt);
 
-      if(err >= time_err_max) {
-        dt *= 0.5;
+      factor = err/time_err_max;
+      if(factor > 1.0) {
+        dt *= 0.9*pow(factor,-0.25);
         time_step_dt = dt;
       }
+      umax = time_getmax(&kq[grid_3d_nn_local*0]);
+      vmax = time_getmax(&kq[grid_3d_nn_local*1]);
+      wmax = time_getmax(&kq[grid_3d_nn_local*2]);
+      bmax = time_getmax(&kq[grid_3d_nn_local*3]);
+      printf("factor = %1.16lf, new dt = %lf, umax = %1.16lf, vmax = %1.16lf, wmax = %1.16lf, bmax = %1.16lf\n", factor, dt, umax, vmax, wmax, bmax);
     }
     time += dt;
-    if(err < 0.1*time_err_max && !is_boundary_step) {
-      time_step_dt *= 2.0;
+    if(factor > 1.89e-4 && !is_boundary_step) {
+      time_step_dt *= 0.9*pow(factor,-0.2);
     }
+    else if(!is_boundary_step) {
+      time_step_dt *= 5.0;
+    }
+
     if(time_step_dt > time_dt) time_step_dt = time_dt;
   }
+
+  diag_write();
 
   return 0;
 }
