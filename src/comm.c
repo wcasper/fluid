a661edb3f202fa3ae06eab5e41e765cfe0da211f
wcasper <wcasper@lanl.gov>
2015-08-13 15:48:40 -0600

    fixed restart write

--- src/comm.c
+++ src/comm.c
@@ -81,6 +81,8 @@ int gather_global_array(void *local, void *global, size_t size) {
       task_n0, //starting index of task
       task_nn; //number of indices in the current task
 
+  int m;
+
   MPI_Status status;
 
   ptrdiff_t idx, num;
@@ -90,8 +92,18 @@ int gather_global_array(void *local, void *global, size_t size) {
   tag3  = 3;
 
   if(my_task == master_task) {
-    num = grid_3d_nn_local*2*size;
-    memcpy(global, local, num);
+    num = grid_2d_nn_local*2*size;
+    if(grid_nd == 2) {
+      idx = (grid_ny/2 + 1)*2*grid_2d_n0_local*size;
+      memcpy(&global[idx], local, num);
+    }
+    else {
+      for(m = 0; m < grid_nz; m++) {
+        idx = (grid_ny/2 + 1)*2*grid_2d_n0_local*size;
+        idx +=(grid_ny/2 + 1)*2*grid_nx*m*size;
+        memcpy(&((char *)global)[idx], local, num);
+      }
+    }
   }
 
   if(my_task == master_task) {
@@ -101,29 +113,44 @@ int gather_global_array(void *local, void *global, size_t size) {
       MPI_Recv(&task_nn,1,MPI_INT,task,
 		tag2,MPI_COMM_WORLD,&status);
 
-      idx = (grid_ny/2 + 1)*2*task_n0*size;
-      if(grid_nd == 3) {
-        idx *= grid_nz;
-      }
       num = task_nn*2*size;
-
-      MPI_Recv(&((char *)global)[idx], num, MPI_CHAR, task,
-		tag3, MPI_COMM_WORLD, &status);
+      if(grid_nd == 2) {
+        idx = (grid_ny/2 + 1)*2*task_n0*size;
+        MPI_Recv(&((char *)global)[idx], num, MPI_CHAR, task,
+                 tag3, MPI_COMM_WORLD, &status);
+      }
+      else {
+        for(m = 0; m < grid_nz; m++) {
+          idx = (grid_ny/2 + 1)*2*task_n0*size;
+          idx +=(grid_ny/2 + 1)*2*grid_nx*m*size;
+          MPI_Recv(&((char *)global)[idx], num, MPI_CHAR, task,
+		   tag3+m, MPI_COMM_WORLD, &status);
+        }
+      }
     }
   }
 
   else {
-    MPI_Send(&grid_2d_n0_local,1,MPI_INT,master_task,
+    task_n0 = grid_2d_n0_local;
+    task_nn = grid_2d_nn_local;
+    MPI_Send(&task_n0,1,MPI_INT,master_task,
              tag1,MPI_COMM_WORLD);
-    MPI_Send(&grid_3d_nn_local,1,MPI_INT,master_task,
+    MPI_Send(&task_nn,1,MPI_INT,master_task,
              tag2,MPI_COMM_WORLD);
 
-    num = grid_3d_nn_local*2*size;
-
-    MPI_Send(local, num, MPI_CHAR,
-             master_task, tag3, MPI_COMM_WORLD);
+    num = grid_2d_nn_local*2*size;
+    if(grid_nd == 2) {
+      MPI_Send(local, num, MPI_CHAR, master_task,
+               tag3, MPI_COMM_WORLD);
+    }
+    else {
+      for(m = 0; m < grid_nz; m++) {
+        idx = grid_2d_nn_local*2*m*size;
+        MPI_Send(&((char *)local)[idx], num, MPI_CHAR,
+                 master_task, tag3+m, MPI_COMM_WORLD);
+      }
+    }
   }
-
 
   return stat;
 }
